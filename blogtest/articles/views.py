bfdd2c3b282a930eb6f133b450cecf1f71ae0e85
DiracSpace <www.ralphlaurenpolo@gmail.com>
2020-08-13 17:08:44 -0500

    displayed retreived data

--- blogtest/articles/views.py
+++ blogtest/articles/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Articles
 
 # function for article list
 def articles_list(request):
+    listofarticles = Articles.objects.all().order_by('date')
     # returning html template render
-    return render(request, 'articles/articles_list.html')
+    return render(request, 'articles/articles_list.html', { 'articles':listofarticles })
