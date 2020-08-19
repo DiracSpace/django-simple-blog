13cda96c85dc6bc93249ae02af567330d9f98fa1
DiracSpace <www.ralphlaurenpolo@gmail.com>
2020-08-19 18:24:37 -0500

    added login, logout and redirect functions

--- blogtest/accounts/urls.py
+++ blogtest/accounts/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    re_path(r'^signup/$', views.usersignup, name="signup")
+    re_path(r'^signup/$', views.usersignup, name="signup"),
+    re_path(r'^login/$', views.userlogin, name="login"),
+    re_path(r'^logout/$', views.userlogin, name='logout'),
 ]
--- blogtest/accounts/views.py
+++ blogtest/accounts/views.py
@@ -1,17 +1,39 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+
+"""
+# 1- if scenario is POST, than generate new user
+# 2- if scenario is GET, generate a new instance
+# 3- if scenario is POST but error, render a new fresh template
+"""
 
 # function for usersignup
 def usersignup(request):
-    # 1- if scenario is POST, than generate new user
-    # 2- if scenario is GET, generate a new instance
-    # 3- if scenario is POST but error, render a new fresh template
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            # user login
+            user = form.save()
+            # user registration and redirection to articles
+            login(request, user)
             return redirect('articles:list')
     else:
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', { 'form':form })
+
+def userlogin(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # user login and redirection to articles
+            user = form.get_user()
+            login(request, user)
+            return redirect('articles:list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', { 'form':form })
+
+def userlogout(request):
+    if request.method == 'POST':
+        logout = (request)
+        return redirect('articles:list')
